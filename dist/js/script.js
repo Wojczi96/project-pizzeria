c6638b93533a56ba78bb8e6460bfacbaa747d422
Wojciech Wyskiel <wojtek12.1.96@wp.pl>
2023-11-10 11:51:17 +0100

    Done calculate price section

--- dist/js/script.js
+++ dist/js/script.js
@@ -83,7 +83,7 @@ const select = {
     }
     initAccordion(){
       const thisProduct = this;
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      // const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       // clickableTrigger.addEventListener('click', function(event){
         thisProduct.accordionTrigger.addEventListener('click', function(event) {
 
@@ -118,24 +118,24 @@ const select = {
       const thisProduct = this;
       const formData = utils.serializeFormToObject(thisProduct.form);
       console.log('formData:', formData);
-      // set price to default price
       let price = thisProduct.data.price;
-
-      // for every category (param)...
       for(let paramId in thisProduct.data.params) {
-        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
 
-        // for every option in this category
         for(let optionId in param.options) {
-          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+          console.log(option);
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+            if(option.default !== true){
+              price += option.price;
+            }
+          } else {
+            if(option.default === true){
+              price -= option.price;
+            }
+          }
         }
       }
-
-      // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
     }
   }
